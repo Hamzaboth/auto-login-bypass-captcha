d59797084c352756cf1fdd0a6ab6d07861071dd1
suborange <ebonavida0@yahoo.com>
2023-10-10 01:41:04 -0700

    initial main setup
    basic browser and page setup;
    need to figure out the secret files/data

--- Source/main.js
+++ Source/main.js
@@ -5,3 +5,50 @@
  * LIKE FOR WRITING A USERNAME AND PASSWORD IN A LOGIN PAGE, AND PRESSING THE LOGIN BUTTON
  * 
  */
+
+/***********************
+ * SECRET IMPORTS 
+ **********************/
+// import { } from 
+// const _SECRETS = require("./secret.json");
+
+/***********************
+ * PUPPETEER IMPORTS 
+ **********************/
+// const puppeteer = require('puppeteer'); // for regular puppeteer - overshadowed by puppeteer-extra
+const puppeteer_extra = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer_extra.use(StealthPlugin); // use the stealth plugin in to help deter bot detection
+
+// FILE OBJECT
+const file_io = require('fs'); // for file input and output
+
+
+// URL CONSTS FOR TESTING
+const boturl = 'https://bot.sannysoft.com/'; // test if you look like a bot or not
+// const url = 
+
+const main = async () => {
+    // SETUP PAGE
+    // const browser = await p_extra.launch({
+    //     headless: false, // dont run headless, to see what is going on in the browser
+    //     args:  ['--disable-web-security', // disable a bunch of shit - might be overkill but something i found
+    //     '--disable-features=IsolateOrigins,site-per-process', 
+    //     '--disable-features=IsolateOrigins,site-per-process,SitePerProcess',
+    //     '--flag-switches-begin --disable-site-isolation-trials --flag-switches-end'], 
+
+    // });
+    // let page = await browser.newPage();
+    // await page.goto(url, {
+    //     waitUntil: "networkidle2",
+    // });
+
+    // testing
+    console.log("USER: " + _SECRETS.USERNAME + "\nPASS"+_SECRETS.PASSWORD);
+
+
+}
+
+
+// CALL MAIN FUNCTION
+main();
